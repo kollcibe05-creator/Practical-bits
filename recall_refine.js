3ff4783433635ebb52305468fabdc87b1d76d212
Collins Kibet <kollcibe05@gmail.com>
2025-12-11 22:29:04 +0300

    converting a value to a string

---
+++ recall_refine.js
@@ -0,0 +1,11 @@
+// const { cacheSignal } = require("react")
+
+console.log("Hello again bitches!!")
+console.log("Shiet adi nimesahau ku run js kwa terminal,,,,,ni mbaya!!")
+
+let greeting = () => "Bonga shit na niende na key ya choo."
+console.log(greeting())
+
+console.log(typeof("string"))
+
+console.log("85".toString())     //changing to a string 
--- testing.js
+++ testing.js
@@ -1378,7 +1378,6 @@ console.log(Palindrome("mom"));
 // console.log(testT("racecar"));
 
 
-<<<<<<< HEAD
 /*
 
 //EFFECTIVELY EXECUTES but according to the Big O notation it isn't//
@@ -1442,8 +1441,6 @@ for (const num of array) {
 }
 }
 
-=======
->>>>>>> parent of c466d70 (Two_sum problem)
 
 
 
