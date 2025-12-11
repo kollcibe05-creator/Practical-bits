5c46e5cfac307e11e04718fd39c4fa38128e326e
Collins Kibet <kollcibe05@gmail.com>
2025-12-12 00:25:54 +0300

    Howdoyousayhiinyourlanguage

--- recall_refine.js
+++ recall_refine.js
@@ -1,7 +1,9 @@
 // const { cacheSignal } = require("react")
-
+/*
+to run js in the terminal ~     $ node <fileName>.js
+*/
 console.log("Hello again bitches!!")
-console.log("Shiet adi nimesahau ku run js kwa terminal,,,,,ni mbaya!!")
+console.log("Shiet hadi nilikuwa nimesahau ku run js kwa terminal,,,,,ni mbaya!!😂")
 
 let greeting = () => "Bonga shit na niende na key ya choo."
 console.log(greeting())
@@ -10,4 +12,36 @@ console.log(typeof("string"))
 
 console.log("85".toString())     //changing to a string 
 
-console.assert(typeof("Whoa" === "string"))
+
+if (typeof("Whoa") === "string"){
+    answer = "Yes"
+}else{
+    answer = "No"
+}
+console.log(answer)
+
+
+list = [1,2,3,4,5,6,7,8,9,10, 21]
+
+let multiplyByFour = list.map(n => n*4)
+
+console.log(multiplyByFour)
+
+console.log('collo'? "Many men" : "Oops")
+
+function evaluate (num1, num2, foo){
+    return foo(num1, num2)
+}
+
+function toBeCalled(num1, num2){
+    return  num1 + num2
+}
+
+// console.log(evaluate(6,4,toBeCalled))
+
+// let anonymous = list.forEach(function(number){
+//     console.log(`number: ${number}`)
+// })
+// console.log(anonymous)
+console.log(list[list.length-1])
+
