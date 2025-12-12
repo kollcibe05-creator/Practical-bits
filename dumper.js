218dd7ef32a6e04f2af2934a46a060f854f71c44
Collins Kibet <kollcibe05@gmail.com>
2025-12-12 18:18:36 +0300

    bienvenue

--- dumper.js
+++ dumper.js
@@ -79,3 +79,54 @@ let joint = list_.concat(arrTwo)
 // console.log(list_)
 console.log(list_.indexOf(21))``
 
+
+// console.log(dict_["spanish"])
+// console.log(dict_["spanish"])
+let keys = Object.keys(dict_)
+// console.log(Object.values(dict_))
+// let float = (parseFloat(15))
+// console.log(float)
+
+
+// for (let key in dict_){
+//     console.log(key[i])
+// }
+
+// console.log(4**2)
+// console.table(dict_)
+
+// console.log(keys.length)
+
+// for (let i = 0; i<=keys.length; i++){
+//     console.log(i)
+// }
+
+// for (let obj in dict_){
+//     console.log(dict_[obj])
+// }
+let i = 0
+
+// while ( i<=keys.length){
+//     console.log(i)
+//     ++i
+// }
+
+// function totalInventory(array){
+//     total = []
+//     for (let item of array){
+//         total.push(array.indexOf(item) + 1)
+//     }
+//     return total
+// }
+
+// console.log(totalInventory(keys))
+
+// reduce = keys.reduce((reducer,item ) => reducer+= keys.indexOf(item), 11 )
+// console.log(reduce)
+// findIncludes = keys.includes("french")
+// find = keys.instance(item => item == "french")
+// console.log(find)
+// console.log("find".repeat(4))
+// console.log(Math.random())
+const today = new Date(2024,6,9,12)
+console.log(today.getMinutes())
--- recall_refine.js
+++ recall_refine.js
@@ -18,53 +18,15 @@ dict_ = {
  welsh: "Croeso",
 }
 
-// console.log(dict_["spanish"])
-// console.log(dict_["spanish"])
-let keys = Object.keys(dict_)
-// console.log(Object.values(dict_))
-// let float = (parseFloat(15))
-// console.log(float)
+function greet(lang){
+   
+    for(let language in dict_){
+        if (language == lang){
+            greeting = dict_[language]
+            return greeting
+        }
+    }
+    return "Welcome"
+}
 
-
-// for (let key in dict_){
-//     console.log(key[i])
-// }
-
-// console.log(4**2)
-// console.table(dict_)
-
-// console.log(keys.length)
-
-// for (let i = 0; i<=keys.length; i++){
-//     console.log(i)
-// }
-
-// for (let obj in dict_){
-//     console.log(dict_[obj])
-// }
-let i = 0
-
-// while ( i<=keys.length){
-//     console.log(i)
-//     ++i
-// }
-
-// function totalInventory(array){
-//     total = []
-//     for (let item of array){
-//         total.push(array.indexOf(item) + 1)
-//     }
-//     return total
-// }
-
-// console.log(totalInventory(keys))
-
-// reduce = keys.reduce((reducer,item ) => reducer+= keys.indexOf(item), 11 )
-// console.log(reduce)
-// findIncludes = keys.includes("french")
-// find = keys.instance(item => item == "french")
-// console.log(find)
-// console.log("find".repeat(4))
-// console.log(Math.random())
-const today = new Date(2024,6,9,12)
-console.log(today.getMinutes())
+console.log(greet("swedish"))
