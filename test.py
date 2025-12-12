1955d41466c1e4162294cc8f38ae677bc0e42f6b
Collins Kibet <kollcibe05@gmail.com>
2025-12-12 21:34:50 +0300

    covered some python advanced stuff

--- test.py
+++ test.py
@@ -3,5 +3,22 @@
 def is_even (n):
     return (n & 1) == 0
 
-print(is_even(5))    
+# print(is_even(5))    
 
+arr = str([1,2,3,4,5,6,7,2,2])
+
+arr_2 = str([1,9,18,4])
+joint = "".join(["hello", "world"])
+# print(joint)
+
+# print("Hello".split())
+
+print(id(arr))
+# print(id(list_))
+
+print(arr.count("2"))
+
+# print(pow(2,3))    or print(2**3)
+
+print(format(123.4567, ".2f"))
+print(globals())
