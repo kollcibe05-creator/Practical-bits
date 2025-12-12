7d6049e4fa8f146f7086cc4dd91e7bc7fc261683
Collins Kibet <kollcibe05@gmail.com>
2025-12-12 18:43:58 +0300

    even no

--- recall_refine.js
+++ recall_refine.js
@@ -4,3 +4,5 @@ arr = [1,2,3,4,5,6,7]
 // console.log(arr)
 // arr.fill(2, 4)
 // console.log(arr)
+
+console.log(arr.at(4))
--- test.py
+++ test.py
@@ -4,3 +4,4 @@ def is_even (n):
     return (n & 1) == 0
 
 print(is_even(5))    
+
