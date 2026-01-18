b07386747979306b71048694b27e032facf70075
Collins Kibet <kollcibe05@gmail.com>
2026-01-18 22:35:31 +0300

    capitalizedSentence

--- recall_refine.js
+++ recall_refine.js
@@ -10,3 +10,28 @@ arr = [1,2,3,4,5,6,7]
 console.log(arr.toString(""))
 
 // console.log("hello".split())
+
+
+let sent = "many men are crazy"
+
+let list = sent.split(" ")
+
+// let capitalizedWord = list.map(word => word.toUpperCase())
+// console.log(capitalized)
+
+function capitalizeWord(word) {
+   return  word.charAt(0).toUpperCase()+ word.slice(1).toLowerCase()
+}
+
+
+console.log(capitalizeWord("malik"))
+
+// console.log("Word".slice(0,1))
+
+
+let capitalizedWords = list.map(word => capitalizeWord(word))
+console.log(capitalizedWords)
+
+let CapitalizedSentence =  capitalizedWords.join(" ")
+
+console.log(CapitalizedSentence)
