64db5c108794caed95ca191d38b4a2df96f734dd
Collins Kibet <kollcibe05@gmail.com>
2026-01-20 22:52:11 +0300

    optional chaining

---
+++ advanced.js
@@ -0,0 +1,8 @@
+const userName = {
+    name: "Collins",
+    title: "God's favorite.",
+    say: "God is my all. I am striving hard to meet his expectations for man.",
+    movie: "Peaky"
+}
+
+console.log(userName.name?.movie =="Peaky" ? "Yes": "No")
