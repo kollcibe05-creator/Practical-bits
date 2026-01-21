3580cafa0949cf05ef77441d6ebdf4692a243fdb
Collins Kibet <kollcibe05@gmail.com>
2026-01-21 21:28:04 +0300

    palindrome init

--- advanced.js
+++ advanced.js
@@ -1,8 +1,39 @@
-const userName = {
-    name: "Collins",
-    title: "God's favorite.",
-    say: "God is my all. I am striving hard to meet his expectations for man.",
-    movie: "Peaky"
-}
 
-console.log(userName.name?.movie =="Peaky" ? "Yes": "No")
+
+
+// const username = {
+//     name: "Collins",
+//     title: "God's favorite.",
+//     say: "God is my all. I am striving hard to meet his expectations for man.",
+//     movie: "Peaky"
+// }
+
+// // console.log(username.name?.movie =="Peaky" ? "Yes": "No")
+
+// function iterate() {
+// for (let i = 0; i<Object.keys(username).length; i++){
+//     console.log(Object.keys(username)[i])
+// }
+// }
+
+// console.log(iterate())
+// // console.log(Object.keys(username).length)
+
+
+
+
+// function isPalindrome(word){
+//     for (let i = 0; i<(word.length)/2; i++){
+//         let j = word.length-1-i
+//         if(word[j] === word[i]){
+//             return true
+//         }else{
+//             return false
+//         }
+//     }
+
+// }
+
+// console.log(isPalindrome("racecar"))
+
+
