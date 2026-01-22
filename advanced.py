4bacfd9ef4f41a37f4051ddbfbaa8b55922e13d1
Collins Kibet <kollcibe05@gmail.com>
2026-01-22 07:15:16 +0300

    evaluate keystrokes

--- advanced.py
+++ advanced.py
@@ -79,23 +79,38 @@
 # print(reverse_string("Coolio"))
 
 
-def evaluate_keystrokes(string):
-    i = len(string) - 1
-    result = ""
-    skip = 0
-    while i >= 0:
-        if string[i] == "<":
-            skip += 1
-            i -= 1
-        else:
-            if skip> 0:
-                i -= skip
-                skip = 0
-            else:
-                result = string[i] + result
-                i -= 1 
-    return result                   
+# def evaluate_keystrokes(string):
+#     i = len(string) - 1
+#     result = ""
+#     skip = 0
+#     while i >= 0:
+#         if string[i] == "<":
+#             skip += 1
+#             i -= 1
+#         else:
+#             if skip> 0:
+#                 i -= skip
+#                 skip = 0
+#             else:
+#                 result = string[i] + result
+#                 i -= 1 
+#     return result                   
 
+
+# print(evaluate_keystrokes("abcd<<<fg<h"))
+
+
+def evaluate_keystrokes(string):
+    stack = []
+    for char in string:
+        if char =="<":
+            if len(stack) != 0:
+                stack.pop()
+        else:
+            stack.append(char)
+    result = ''
+    while stack:
+        result = stack.pop() + result
+    return result                    
 
 print(evaluate_keystrokes("abcd<<<fg<h"))
-
